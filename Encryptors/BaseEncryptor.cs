190a835bb6b33f956cfc2ef2607178bb629a3fa1
Torben Kneesch <Kneesch@gmx.de>
2022-11-30 20:24:25 +0100

    Set up project with simple encryptor

---
+++ Encryptors/BaseEncryptor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoAlgorithms.Encryptors
+{
+    public class BaseEncryptor
+    {
+        public long TimeInMsForLastRun
+        {
+          get { return _timeForLastRun; }
+        }
+        private long _timeForLastRun;
+
+        protected virtual string Encrypt(string inputText, string key)
+            => throw new NotImplementedException("Encrypt method in base class should not be called.");
+
+        public string RunEncryption(string inputText, string key)
+        {
+            Stopwatch stopwatch = new ();
+            stopwatch.Start();
+
+            string outputText = Encrypt(inputText, key);
+
+            stopwatch.Stop();
+            _timeForLastRun = stopwatch.ElapsedMilliseconds;
+
+            return outputText;
+        }
+
+    }
+}
---
+++ Encryptors/CesarCipherEncryptor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoAlgorithms.Encryptors
+{
+    public class CesarCipherEncryptor : BaseEncryptor
+    {
+        const string _charset = "abcdefghijklmnopqrstuvwxyzäöüßABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ";
+        protected override string Encrypt(string inputText, string key)
+        {
+            int shiftNumber = KeyStringToShiftNumber(key);
+            string outputText = string.Empty;
+            for (int i=0; i<inputText.Length; i++)
+            {
+                int charIndex = _charset.IndexOf(inputText[i]);
+                int newIndex = (charIndex + shiftNumber) % _charset.Length;
+                outputText += _charset[newIndex];
+            }
+            return outputText;
+        }
+
+        private int KeyStringToShiftNumber(string keyString)
+        {
+            return int.Parse(keyString);
+        }
+    }
+}
